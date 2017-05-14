67f35105872cdf538a3da477cacf4fae177a999b
David Corkett <dcorkett@gmail.com>
2017-05-14 21:10:32 +0100

    added logic to clock

--- app/js/controllers/record.controller.js
+++ app/js/controllers/record.controller.js
@@ -1,25 +1,37 @@
-function ClockController($timeout){
+function ClockController($interval){
   var controller = this;
-  controller.canStart = false;
   controller.startSound = false;
-  controller.startMusic = false;
-
 
   controller.toggleStart = function() {
-    controller.canStart = !controller.canStart;
     controller.startSound = !controller.startSound;
-
-    $timeout(function () {
-      controller.startMusic = !controller.startMusic;
-    }, 200);
   };
 
 
+  controller.tick = function() {
+    controller.date = new Date();
+    controller.seconds = controller.date.getUTCSeconds();
+    controller.minutes = controller.date.getUTCMinutes();
+    controller.hours = controller.date.getUTCHours() + 1;
+    controller.secondsHandAngle = controller.seconds * 6;
+    controller.minutesHandAngle = controller.minutes * 6;
+    controller.hoursHandAngle = (controller.hours * 30) + (controller.minutes * 0.5);
+
+    controller.secondsHandStyle = {
+      'transform': `rotate(${controller.secondsHandAngle}deg)`
+    };
+    controller.minutesHandStyle = {
+      'transform': `rotate(${controller.minutesHandAngle}deg)`
+    };
+    controller.hoursHandStyle = {
+      'transform': `rotate(${controller.hoursHandAngle}deg)`
+    };
+  };
+
+  ClockController.$inject = ['$interval'];
 
   function init() {
-
+    $interval(controller.tick, 1000);
   }
-
 
   init();
 }
