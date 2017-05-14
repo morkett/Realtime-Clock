3cae04a772acec98a74ba7d2c8b384312ca799c5
David Corkett <dcorkett@gmail.com>
2017-05-14 20:55:47 +0100

    initial setup

---
+++ app/js/controllers/record.controller.js
@@ -0,0 +1,31 @@
+function ClockController($timeout){
+  var controller = this;
+  controller.canStart = false;
+  controller.startSound = false;
+  controller.startMusic = false;
+
+
+  controller.toggleStart = function() {
+    controller.canStart = !controller.canStart;
+    controller.startSound = !controller.startSound;
+
+    $timeout(function () {
+      controller.startMusic = !controller.startMusic;
+    }, 200);
+  };
+
+
+
+  function init() {
+
+  }
+
+
+  init();
+}
+
+
+
+angular
+  .module('ClockApp')
+  .controller('ClockController', ClockController);
